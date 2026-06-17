cb6b28a4ae12b8bbeb645caca35a26dfd0930dbd
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-06-17 17:19:36 -0300

    feat: add frontend URL to environment variables and configure CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,11 @@ import { Score } from "./models/Score";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  }),
+);
 app.use(express.json());
 
 // 🔗 Conexão com o MongoDB
